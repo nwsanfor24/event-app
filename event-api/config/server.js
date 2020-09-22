69a5ebd9bbea5bb634a40163d78c1760a60acd64
Nathan Sanford <nwsanfor24@gmail.com>
2020-09-22 19:10:12 -0400

    created an event api using strapi.js

---
+++ event-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c31d1927156c09b060b5582d4ac20059'),
+    },
+  },
+});
---
+++ event-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cd683a69-c8cc-488f-a407-c7ea4f9c09ad'
+};
